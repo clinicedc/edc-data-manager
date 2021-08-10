c9c1f573be3628db66dd7997596a936f90d9f8b0
erikvw <ew2789@gmail.com>
2021-08-10 00:38:11 -0500

    bypass  for inline models

--- edc_data_manager/models/signals.py
+++ edc_data_manager/models/signals.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from edc_lab.utils import get_panel_model
+from edc_model.utils import is_inline_model
 
 from ..rule import RuleRunner
 from .data_query import DataQuery
@@ -30,39 +31,40 @@ def update_query_on_crf(sender, instance, raw, **kwargs):
             except AttributeError:
                 pass
             else:
-                subject_identifier = instance.visit.appointment.subject_identifier
-                visit_schedule = QueryVisitSchedule.objects.get(
-                    visit_schedule_name=instance.visit.appointment.visit_schedule_name,
-                    schedule_name=instance.visit.appointment.schedule_name,
-                    visit_code=instance.visit.appointment.visit_code,
-                )
-                try:
-                    instance.panel
-                except AttributeError:
-                    data_queries = DataQuery.objects.filter(
-                        id__isnull=False,
-                        rule_generated=True,
-                        subject_identifier=subject_identifier,
-                        visit_schedule=visit_schedule,
-                        data_dictionaries__model=sender._meta.label_lower,
-                    )
-                else:
-                    data_queries = DataQuery.objects.filter(
-                        id__isnull=False,
-                        rule_generated=True,
-                        subject_identifier=subject_identifier,
-                        visit_schedule=visit_schedule,
-                        requisition_panel=get_panel_model().objects.get(
-                            name=instance.panel.name
-                        ),
-                    )
-                for data_query in data_queries:
-                    query_rule = QueryRule.objects.get(reference=data_query.rule_reference)
-                    runner = RuleRunner(query_rule)
-                    runner.run_one(
-                        subject_identifier=subject_identifier,
-                        visit_schedule_name=instance.visit.appointment.visit_schedule_name,  # noqa
+                if not is_inline_model(instance):
+                    subject_identifier = instance.visit.appointment.subject_identifier
+                    visit_schedule = QueryVisitSchedule.objects.get(
+                        visit_schedule_name=instance.visit.appointment.visit_schedule_name,
                         schedule_name=instance.visit.appointment.schedule_name,
                         visit_code=instance.visit.appointment.visit_code,
-                        timepoint=instance.visit.appointment.timepoint,
                     )
+                    try:
+                        instance.panel
+                    except AttributeError:
+                        data_queries = DataQuery.objects.filter(
+                            id__isnull=False,
+                            rule_generated=True,
+                            subject_identifier=subject_identifier,
+                            visit_schedule=visit_schedule,
+                            data_dictionaries__model=sender._meta.label_lower,
+                        )
+                    else:
+                        data_queries = DataQuery.objects.filter(
+                            id__isnull=False,
+                            rule_generated=True,
+                            subject_identifier=subject_identifier,
+                            visit_schedule=visit_schedule,
+                            requisition_panel=get_panel_model().objects.get(
+                                name=instance.panel.name
+                            ),
+                        )
+                    for data_query in data_queries:
+                        query_rule = QueryRule.objects.get(reference=data_query.rule_reference)
+                        runner = RuleRunner(query_rule)
+                        runner.run_one(
+                            subject_identifier=subject_identifier,
+                            visit_schedule_name=instance.visit.appointment.visit_schedule_name,  # noqa
+                            schedule_name=instance.visit.appointment.schedule_name,
+                            visit_code=instance.visit.appointment.visit_code,
+                            timepoint=instance.visit.appointment.timepoint,
+                        )
