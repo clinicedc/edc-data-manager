1cbcfd7cfc716461c7afd21657e85e3bf8863b0e
erikvw <ew2789@gmail.com>
2022-06-30 23:09:53 +0300

    add message to user about data manager role in admin

--- edc_data_manager/data_manager_modeladmin_mixin.py
+++ edc_data_manager/data_manager_modeladmin_mixin.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.messages import get_messages
+from django.utils.html import format_html
 
 from edc_data_manager.auth_objects import DATA_MANAGER_ROLE
 
@@ -12,6 +15,18 @@ class DataManagerModelAdminMixin:
         expanded_view_roles = getattr(
             settings, "EDC_DATA_MANAGER_EXPANDED_VIEW_ROLES", [DATA_MANAGER_ROLE]
         )
+        storage = get_messages(request)
+        roles = " ".join(expanded_view_roles)
+        my_message = format_html(
+            "Your are being shown data from all sites but will only be able to change "
+            "data for the current site. To view data for one site, use the 'By site' "
+            "filter on the right sidebar. This feature is enabled because you are a "
+            f"member of one of these roles: <i>{roles}</i>.",
+            fail_silently=True,
+        )
+        if not [message for message in storage if message.message == my_message]:
+            messages.warning(request, my_message)
+
         roles = [r.name for r in request.user.userprofile.roles.all()]
         if list(set(expanded_view_roles) & set(roles)):
             site_ids = [s.id for s in request.user.userprofile.sites.all()]
